d9d0887b242c691ed6f743a6fb7b696d8df4becf
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-07-05 20:18:28 +0900

    30 - users#login,login_form ログイン機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,17 @@ end
   end
 
   def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/quests/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email =  params[:email]
+      @password = params[:password]
+      render("users/login_form")
   end
+end
 
 end
