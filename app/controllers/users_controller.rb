41e7e2d14c0198f9c6d9c2fd7adb4f1a691faa36
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-07-06 20:13:10 +0900

    32 - ログイン中のユーザー名を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email], image_name: "default_user.png")
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password] , image_name: "default_user.png")
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
-  else
-    render("/users/new")
+   else
+      render("/users/new")
   end
 end
 
