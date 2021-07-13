61e63e64eb305d8d635369f64283ac2ade83415c
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-07-13 21:10:44 +0900

    43 - パスワードのハッシュ化処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true}
+  has_secure_password
 
   def quests
     return Quest.where(user_id: self.id)
